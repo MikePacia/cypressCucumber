bf357f8489244bc7e8cf23f719da666d33e0f98a
MikePacia <mikepacia040@gmail.com>
2024-10-04 08:47:12 +0800

    first commit of cucumber

---
+++ cypress/support/step_definitions/login_steps.js
@@ -0,0 +1,42 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
+
+
+Given("I have launched the swag labs application", function () {
+    cy.visit('https://www.saucedemo.com/v1/index.html')
+})
+
+When("I enter the correct username and password", function () {
+    const username = '#user-name'
+    const pword = '#password'
+
+    cy.get(username).type('standard_user')
+    cy.get(pword).type('secret_sauce')
+})
+
+When("I click the log in button", function () {
+    const loginbtn = '#login-button'
+    cy.get(loginbtn).click()
+})
+Then("I should land on the products page", function () {
+    cy.get('div.product_label').should('have.text', 'Products')
+})
+
+
+
+//LOG IN FAILURE
+
+
+When(`I enter the incorrect username and password`, () => {
+    const username = '#user-name'
+    const pword = '#password'
+
+    cy.get(username).type('standard_user')
+    cy.get(pword).type('secret_sauce122')
+});
+
+
+Then(`I should get the error message {string}`, (message) => {
+    const error = 'h3'
+    cy.get(error).contains(message)
+});
+
